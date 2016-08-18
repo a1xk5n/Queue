459f73c97d1043055e9d3d785946a216e8ca8eb7
Alex Kashkan <alexei.kashkan@gmail.com>
2016-08-18 22:40:01 +0300

    Create a max-heap

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,39 +2,97 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
+		this.cursize = 0;
 	}
 
 	push(data, priority) {
-		
+		var nodeForPush = new Node(data, priority);
+		this.insertNode(nodeForPush);
+		this.shiftNodeUp(nodeForPush);
+		this.cursize++;
 	}
 
 	pop() {
-		
+		if(this.root === null) return;
+		var detached = this.detachRoot();
+		this.cursize --;
+		this.restoreRootFromLastInsertedNode(detached);
+		this.shiftNodeDown(this.root);
+		return detached.data;
 	}
 
 	detachRoot() {
-		
+		var detached = this.root;
+		if (this.root.left !== null)
+			this.root.left.parent = null;
+		if (this.root.right !== null)
+			this.root.right.parent = null;
+		this.root = null;
+		return detached;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+		var last = this.parentNodes[this.parentNodes.length - 1];
+		var lastParent = last.parent;
+		last.remove();
+		if (last === this.parentNodes[0]) {
+			this.root = null;
+			this.parentNodes.shift();
+			return;
+		}
+		if (last === detached.left) {
+			last.left = null;
+			last.right = null;
+			last.parent = null;
+			this.parentNodes.shift();
+		} else if (last === detached.right) {
+			last.right = null;
+			last.parent = null;
+			last.left = detached.left;
+			last.left.parent = last;
+			this.parentNodes.unshift(last);
+			this.parentNodes.pop();
+		} else {
+			last.left = detached.left;
+			last.left.parent = last;
+			last.right = detached.right;
+			last.right.parent = last;
+			if (this.parentNodes.indexOf(lastParent) === -1)
+				this.parentNodes.unshift(lastParent);
+			this.parentNodes.pop();		
+		}
+		this.root = last;
 	}
 
 	size() {
-		
+		return this.cursize;
 	}
 
 	isEmpty() {
-		
+		if(this.cursize === 0) return true;
+		else false; 
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
+		this.cursize = 0;
 	}
 
 	insertNode(node) {
-		
+		if(this.root === null) {
+			this.root = node;
+			this.parentNodes.push(node);
+		}	
+		else{
+			this.parentNodes[0].appendChild(node);
+			this.parentNodes.push(node);
+			if (this.parentNodes[0].right !== null) {
+				this.parentNodes.shift();
+			}
+		}
 	}
 
 	shiftNodeUp(node) {
